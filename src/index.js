98f86b8608b8e6582c6118ff8a0daa03a5c342a5
Francisco Barros <francisco.ifnmg@gmail.com>
2021-07-19 17:58:00 -0300

    muita coisas, várias

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+import axios from 'axios'
+
+axios.get('https://minhastarefas-api.herokuapp.com/tarefas',{
+    headers:{'x-tenant-id':'francisco@gmail.com'}
+}).then( resposta => {
+    console.log(resposta.data);
+})
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
